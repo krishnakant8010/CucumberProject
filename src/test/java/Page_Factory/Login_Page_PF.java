98151841d4e1e7bf98b70ca4bb42620cf9803bc4
krishnakant8010 <kantkrishna2016@gmail.com>
2024-04-16 12:58:43 +0530

    login code change

--- src/test/java/Page_Factory/Login_Page_PF.java
+++ src/test/java/Page_Factory/Login_Page_PF.java
@@ -9,36 +9,36 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Login_Page_PF {
 
-	@FindBy(xpath="//input[@placeholder='Email']")
-	@CacheLookup
-	WebElement txt_username;
-	
-	@FindBy(xpath="//input[@placeholder='Password']")
-	WebElement txt_password;
-	
-	@FindBy(xpath="//span[contains(text(),'SIGN IN')]")
-	WebElement btn_Login;
-	
-	WebDriver driver;
-	
-	public Login_Page_PF(WebDriver driver) {
-		this.driver =driver;
-		PageFactory.initElements(driver, this);
-		
-	}
-	
-	public void enterUsername(String username) {
-		txt_username.sendKeys(username);
-		System.out.println(txt_username);
-	}
-	
-	public void enterPassword(String password) {
-		txt_password.sendKeys(password);
-	}
-	
-	public void clickOnLogin() {
-		btn_Login.click();
-	}
+//	@FindBy(xpath="//input[@placeholder='Email']")
+//	@CacheLookup
+//	WebElement txt_username;
+//	
+//	@FindBy(xpath="//input[@placeholder='Password']")
+//	WebElement txt_password;
+//	
+//	@FindBy(xpath="//span[contains(text(),'SIGN IN')]")
+//	WebElement btn_Login;
+//	
+//	WebDriver driver;
+//	
+//	public Login_Page_PF(WebDriver driver) {
+//		this.driver =driver;
+//		PageFactory.initElements(driver, this);
+//		
+//	}
+//	
+//	public void enterUsername(String username) {
+//		txt_username.sendKeys(username);
+//		System.out.println(txt_username);
+//	}
+//	
+//	public void enterPassword(String password) {
+//		txt_password.sendKeys(password);
+//	}
+//	
+//	public void clickOnLogin() {
+//		btn_Login.click();
+//	}
 
 	
 }
